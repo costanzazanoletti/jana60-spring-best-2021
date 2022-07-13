3fbc0fe2431c0f5df48c616de8a34052967dbd7c
costanzazanoletti <zanoletti.cos@gmail.com>
2022-07-13 13:11:16 +0200

    first commit

---
+++ src/main/java/jana60/controller/MainController.java
@@ -0,0 +1,34 @@
+package jana60.controller;
+
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+  /* metodo per la home */
+  @GetMapping
+  public String index() {
+    return "index";
+  }
+
+  /* metodo per i movies */
+  @GetMapping("/movies")
+  public String movies(Model model) {
+    // lista dei best movies
+    List<String> movieList =
+        List.of("My Salinger Year", "Palmer", "Blu Bayou", "Cruella", "Percy VS Goliath");
+    model.addAttribute("movieList", movieList);
+    return "movies";
+  }
+
+  /* metodo per le songs */
+  @GetMapping("/songs")
+  public String songs() {
+    return "songs";
+  }
+}
